096fd202b7d498e3112a8f937b81445d373d631e
Maxizzle <51535019+Maxizzle@users.noreply.github.com>
2019-11-01 11:39:28 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# SunglassHouse
+#SunglassHouse
+
+#SunglassHouse
+
+The idea behind the site is just your typical e-commerce platform.
+I went with sunglasses cause I think they’re one of the most important accessories 
+you can carry because one, you carry them on your face so it is the first thing people 
+see when they come in contact with you so it matters how they look and more importantly how
+they look on you.
+Second , they help with visibility on bright and sunny days while also protecting your eyes from
+harmful UV rays eminating from our personal star.  
+
+![Sunglasshouse](/images/giphy-1.gif)
+
+
+
+
+It was a bit of struggle being that was my first project, trying to combine what we've accumulated
+in just two weeks to put together a picture that not only is appealing but aslo functioning.
+Nonetheless is was fun and i really enjoyed doing it.
--- main.js
+++ main.js
@@ -1,1 +1,117 @@
 
+
+let modal = document.getElementById('myModal');
+let button = document.getElementById('myButton');
+let span = document.getElementsByClassName('close')[0];
+
+button.onclick = function() {
+    modal.style.display = 'block';
+
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if(event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function (d){
+// let itemclassName = 'carousel-photo';
+// items = d.document.querySelectorAll(itemclassName),
+// totalItems = items.lenght
+// slide = 0
+// moving = true
+// }
+
+// function setInittialClasses() {
+//     items[totalItems-1].classList.add('prev');
+//     items[0].classList.add('active');
+//     items[1].classList.add('next')
+// }
+
+// function setEventListeners(){
+//     let next = d.getElementByclassName('carousel-photo-next')[0]
+//     prev = d.getElementByclassName('carousel-button-prev')[0;
+    
+//     next.addEventListener('click, moveNext');
+// prev.addEventListener('click', moveprev)]
+// }
+
+// function moveNext(){
+//     if(!moving) {
+//         if(slide ===(totalItems-1)){
+//             slide = 0
+//         }else {
+//             slide++;
+//         }
+//     }
+// }
+
+// function movePrev(){
+//     if(!moving) {
+//         if(slide=== 0) {
+//             slide = (totalItems-1);
+//         } else {
+//             slide--;
+//         }
+//     }
+// }
+
+// function disableInteraction(){
+//     moving = true;
+
+//     setTiemout(function(){
+//         moving = false
+//     }, 500)
+// }
+
+// function moveCarouselTo(slide) {
+//     if(!moving) {
+//         disableInteraction();
+
+//         let newPrevious = slide -1,
+//         newNext = slide +1,
+//         oldPrevious = slide -2,
+//         oldNext = slide + 2;
+
+//         if((totalItems - 1) > 3) {
+
+//             if(newPrevious <= 0) {
+//                 oldPrevious = (totalItems -1)
+
+//             } else if (newNext >= (totalItems-1)) {
+//              oldNext = 0;
+//             }
+//             items[oldPrevious].className = itemclassName;
+//             items[oldNext].className = itemclassName;
+
+//             items[newPrevious].className = itemclassName + 'preve';
+//             items[slide].className = itemclassName + 'next'
+//         }
+//     }
+// }
+
+// function initCarousel() {
+//     setInittialClasses();
+//     setEventListeners();
+// }
+// initCarousel();
